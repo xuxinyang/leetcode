37875159998e5171cd696d22c226baacea1656b0
xuxinyang <1029644978@qq.com>
2020-10-28 20:21:32 +0800

    resolved(19, 20, 21)

--- 19.删除链表的倒数第n个节点.js
+++ 19.删除链表的倒数第n个节点.js
@@ -46,7 +46,25 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    
+    let dummy = new ListNode();
+    dummy.next = head;
+
+    let n1 = dummy;
+    let n2 = dummy;
+
+    for (let i = 0; i <= n; i++) {
+        n2 = n2.next;
+    }
+
+    while (n2 !== null) {
+        n1 = n1.next;
+        n2 = n2.next;
+    }
+
+    n1.next = n1.next.next;
+
+    return dummy.next;
+
 };
 // @lc code=end
 
---
+++ 21.合并两个有序链表.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (64.59%)
+ * Likes:    1345
+ * Dislikes: 0
+ * Total Accepted:    398.2K
+ * Total Submissions: 616.5K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let curr = new ListNode();
+    let dummy = curr;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            curr.next = l1;
+            l1 = l1.next;
+        } else {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+
+    if (l1 !== null) {
+        curr.next = l1;
+    }
+
+    if (l2 !== null) {
+        curr.next = l2;
+    }
+
+    return dummy.next;
+};
+// @lc code=end
+
