f38a4019dfa0acedadf378c2fc6b10d9daf541a4
xuxinyang <1029644978@qq.com>
2022-03-29 01:20:18 +0800

    fix 14,16,22,23

---
+++ 18.四数之和.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=cpp
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        if (nums.size() < 4) {
+            return res;
+        }
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.size() - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1, right = nums.size() - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        res.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
