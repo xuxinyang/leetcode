a55a3f28c26414d620ae9f66839f2bf6f066e58a
xuxinyang <1029644978@qq.com>
2022-04-04 10:42:51 +0800

    fixed 33, 34

---
+++ 33.搜索旋转排序数组.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=cpp
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return -1;
+        int l = 0, r = n - 1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[l] <= nums[mid])
+            {
+                if (nums[l] <= target && target < nums[mid])
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] < target && target <= nums[r])
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
---
+++ 34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return {-1, -1};
+        int l = 0, r = n - 1;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+            {
+                int left = mid, right = mid;
+                while (left >= 0 && nums[left] == target) --left;
+                while (right < n && nums[right] == target) ++right;
+                return {left + 1, right - 1};
+            }
+            else if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return {-1, -1};
+    }
+};
+// @lc code=end
+
