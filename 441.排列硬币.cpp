17767b197b3b655f7da37ae89f9d15cec650679f
xuxinyang <1029644978@qq.com>
2022-04-06 13:04:29 +0800

    fixed 441 other method!

--- 441.排列硬币.cpp
+++ 441.排列硬币.cpp
@@ -8,16 +8,7 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int l = 1, r = n;
-        while (l < r) {
-            int mid = l + (r - l + 1) / 2;
-            if ((long long)mid * (mid + 1)  <= (long long)2 * n) {
-                l = mid;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return l;
+        return (int)((sqrt((long long) 8 * n + 1) - 1) / 2);
 
     }
 };
