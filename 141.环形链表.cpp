6459c119a3e17ef992c54f82cdca14552ef10f3e
xuxinyang <1029644978@qq.com>
2022-05-03 11:11:57 +0800

    fixed 141, 203

---
+++ 141.环形链表.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) return false;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
