5c3de884c7fdcc61e51c33ff6bea013098e52d6a
xuxinyang <1029644978@qq.com>
2022-05-04 10:24:59 +0800

    fixed 1823

--- 1823.找出游戏的获胜者.cpp
+++ 1823.找出游戏的获胜者.cpp
@@ -8,18 +8,8 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int> q;
-        for (int i = 1; i <= n; ++i) {
-            q.emplace(i);
-        }
-        while (q.size() > 1) {
-            for (int i = 1; i < k; i++) {
-                q.emplace(q.front());
-                q.pop();
-            }
-            q.pop();
-        }
-        return q.front();
+        if (n == 1) return 1;
+        return (k + findTheWinner(n - 1, k) - 1) % n + 1;
     }
 };
 // @lc code=end
