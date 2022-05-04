27bedb509021ab0a9066c66f961d2e1e50af30ec
xuxinyang <1029644978@qq.com>
2022-05-04 11:03:10 +0800

    fixed 1091

--- 1091.二进制矩阵中的最短路径.cpp
+++ 1091.二进制矩阵中的最短路径.cpp
@@ -6,36 +6,34 @@
 
 // @lc code=start
 class Solution {
-    const int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-    const int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 public:
+    int result= 1;
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (n == 0) return 0;
-        int m = grid[0].size();
-        if (m == 0) return 0;
-        if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        int step = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; ++i) {
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-                for (int j = 0; j < 8; ++j) {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if (grid[nx][ny] == 1) continue;
-                    if (nx == n-1 && ny == m-1) return step + 1;
-                    grid[nx][ny] = 1;
-                    q.push({nx, ny});
+        if(grid[0][0]!=0) return -1;
+        if(grid.size()==1) return 1;
+        int n=grid[0].size();
+        int d[8][2] = {{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};
+        queue<pair<int,int>> vec;
+        vec.push({0,0});
+        while(!vec.empty()){
+            int len=vec.size();
+            for(int i=0;i<len;i++){
+                auto a = vec.front();    
+                vec.pop();           
+                int x = a.first;
+                int y = a.second;
+                for(int j = 0;j<8;j++){
+                    int xx = x+d[j][0]; //依次遍历八个方向（横坐标）
+                    int yy = y+d[j][1]; //依次遍历八个方向（纵坐标）
+                    if(xx>=0 && xx<n && yy>=0 && yy<n && grid[xx][yy]==0) {
+                        if(xx==n-1 && yy==n-1)
+                            return result+1; 
+                        vec.push({xx,yy});
+                        grid[xx][yy]=1;
+                    }
                 }
             }
-            ++step;
+            result+=1;
         }
         return -1;
     }
