88a81040eb77adca20a46b8b35dbe226e4eb9278
xuxinyang <1029644978@qq.com>
2022-05-08 10:14:22 +0800

    fixed 442

---
+++ 442.数组中重复的数据.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=442 lang=cpp
+ *
+ * [442] 数组中重复的数据
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.push_back(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
