b38a788c0154c34484586cad064aa9d8389fecbf
xuxinyang <1029644978@qq.com>
2022-05-12 10:12:50 +0800

    fixed 673

---
+++ 673.最长递增子序列的个数.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=673 lang=cpp
+ *
+ * [673] 最长递增子序列的个数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size(), maxLen = 0, ans = 0;
+        vector<int> dp(n), cnt(n);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            cnt[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                ans = cnt[i];
+            } else if (dp[i] == maxLen) {
+                ans += cnt[i];
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
