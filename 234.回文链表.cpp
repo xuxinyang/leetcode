018a30ebab9fe418abec134225d7c8bc467ce1ac
xuxinyang <1029644978@qq.com>
2022-05-12 10:30:05 +0800

    fixed 155, 234, 384

---
+++ 234.回文链表.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=cpp
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (fast) {
+            slow = slow->next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        while (slow) {
+            if (slow->val != fast->val) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return true;
+    }
+private:
+    ListNode* reverse(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
+// @lc code=end
+
---
+++ 384.打乱数组.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=384 lang=cpp
+ *
+ * [384] 打乱数组
+ */
+
+// @lc code=start
+class Solution {
+private:
+    vector<int> v;
+public:
+    Solution(vector<int>& nums) {
+        v = nums;
+    }
+    
+    vector<int> reset() {
+        return v;
+    }
+    
+    vector<int> shuffle() {
+        int size = v.size();
+        vector<int> ret(v.begin(), v.end());
+        for (int i = size; i; i--) {
+            swap(ret[random()%i], ret[i-1]);
+        }
+        return ret;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
+// @lc code=end
+
