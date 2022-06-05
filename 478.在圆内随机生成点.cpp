f0dea8ecfc84d1053a986cea8c12eb742ddf7baf
xuxinyang <1029644978@qq.com>
2022-06-05 22:00:26 +0800

    fixed 478

---
+++ 478.在圆内随机生成点.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=478 lang=cpp
+ *
+ * [478] 在圆内随机生成点
+ */
+
+// @lc code=start
+class Solution {
+private:
+    mt19937 gen{random_device{}()};
+    uniform_real_distribution<double> dis;
+    double xc, yc, r;
+
+public:
+    Solution(double radius, double x_center, double y_center): dis(-radius, radius), xc(x_center), yc(y_center), r(radius) {}
+    
+    vector<double> randPoint() {
+        while (true) {
+            double x = dis(gen), y = dis(gen);
+            if (x * x + y * y <= r * r) {
+                return {xc + x, yc + y};
+            }
+        }
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(radius, x_center, y_center);
+ * vector<double> param_1 = obj->randPoint();
+ */
+// @lc code=end
+
