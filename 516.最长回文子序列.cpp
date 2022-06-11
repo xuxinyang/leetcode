05e5e7497e13a3f999f4fb6af120b5161f918005
xuxinyang <1029644978@qq.com>
2022-06-11 22:09:11 +0800

    fixed 373, 516

--- 516.最长回文子序列.cpp
+++ 516.最长回文子序列.cpp
@@ -8,21 +8,23 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        if (n == 0) return 0;
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= n; ++i) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = n - 1; i >= 0; i--) {
             dp[i][i] = 1;
-            for (int j = i - 1; j >= 1; --j) {
-                if (s[i - 1] == s[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 2;
+            char c1 = s[i];
+            for (int j = i + 1; j < n; j++) {
+                char c2 = s[j];
+                if (c1 == c2) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
                 } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return dp[n][1];
+        return dp[0][n - 1];
     }
 };
+
 // @lc code=end
 
