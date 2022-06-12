077fdf879e0d7ae82e4fdc31edc396893db1dd1e
xuxinyang <1029644978@qq.com>
2022-06-12 09:03:02 +0800

    fixed 44, 289

--- 44.通配符匹配.cpp
+++ 44.通配符匹配.cpp
@@ -8,20 +8,25 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        int m = s.size();
+        int n = p.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         dp[0][0] = true;
         for (int i = 1; i <= n; ++i) {
             if (p[i - 1] == '*') {
-                dp[0][i] = dp[0][i - 2];
+                dp[0][i] = true;
+            }
+            else {
+                break;
             }
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i][j - 2] || (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j];
-                } else {
-                    dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '.') && dp[i - 1][j - 1];
+                    dp[i][j] = dp[i][j - 1] | dp[i - 1][j];
+                }
+                else if (p[j - 1] == '?' || s[i - 1] == p[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 }
             }
         }
