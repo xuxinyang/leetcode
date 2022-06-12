5507e88cdb663b69bec131e1b8abb335e62e125a
xuxinyang <1029644978@qq.com>
2022-06-12 08:46:55 +0800

    fixed 890

---
+++ 890.查找和替换模式.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=890 lang=cpp
+ *
+ * [890] 查找和替换模式
+ */
+
+// @lc code=start
+class Solution {
+    bool match(string &word, string &pattern) {
+        unordered_map<char, char> mp;
+        for (int i = 0; i < word.length(); ++i) {
+            char x = word[i], y = pattern[i];
+            if (!mp.count(x)) {
+                mp[x] = y;
+            } else if (mp[x] != y) { // word 中的同一字母必须映射到 pattern 中的同一字母上
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    vector<string> findAndReplacePattern(vector<string> &words, string &pattern) {
+        vector<string> ans;
+        for (auto &word: words) {
+            if (match(word, pattern) && match(pattern, word)) {
+                ans.emplace_back(word);
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
