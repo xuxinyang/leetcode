6a23a0bbd2f8a5b869065974356306efe87d8a5e
xuxinyang <1029644978@qq.com>
2022-06-12 09:11:42 +0800

    fixed 355, 407

--- 407.接雨水-ii.cpp
+++ 407.接雨水-ii.cpp
@@ -5,45 +5,40 @@
  */
 
 // @lc code=start
+typedef pair<int,int> pii;
+
 class Solution {
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
 public:
-    int trapRainWater(vector<vector<int>>& heightMap) {
-        if (heightMap.empty() || heightMap[0].empty()) return 0;
-        int m = heightMap.size(), n = heightMap[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
+    int trapRainWater(vector<vector<int>>& heightMap) {  
+        if (heightMap.size() <= 2 || heightMap[0].size() <= 2) {
+            return 0;
+        }  
+        int m = heightMap.size();
+        int n = heightMap[0].size();
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        vector<vector<bool>> visit(m, vector<bool>(n, false));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.push({heightMap[i][j], i * n + j});
+                    visit[i][j] = true;
+                }
+            }
+        }
         int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
-                    visited[i][j] = 1;
-                }
-            }
-        }
-        queue<pair<int, int>> q;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 0) {
-                    q.push(make_pair(i, j));
-                    visited[i][j] = 1;
-                }
-            }
-        }
-        int h = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int x = q.front().first, y = q.front().second;
-                q.pop();
-                h = max(h, heightMap[x][y]);
-                for (int k = 0; k < 4; k++) {
-                    int nx = x + dx[k], ny = y + dy[k];
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && visited[nx][ny] == 0) {
-                        visited[nx][ny] = 1;
-                        q.push(make_pair(nx, ny));
-                        res += max(0, h - heightMap[nx][ny]);
+        int dirs[] = {-1, 0, 1, 0, -1};
+        while (!pq.empty()) {
+            pii curr = pq.top();
+            pq.pop();            
+            for (int k = 0; k < 4; ++k) {
+                int nx = curr.second / n + dirs[k];
+                int ny = curr.second % n + dirs[k + 1];
+                if( nx >= 0 && nx < m && ny >= 0 && ny < n && !visit[nx][ny]) {
+                    if (heightMap[nx][ny] < curr.first) {
+                        res += curr.first - heightMap[nx][ny]; 
                     }
+                    visit[nx][ny] = true;
+                    pq.push({max(heightMap[nx][ny], curr.first), nx * n + ny});
                 }
             }
         }
