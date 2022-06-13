50317cabf5066eb709358027b33227d035967cf8
xuxinyang <1029644978@qq.com>
2022-06-13 08:58:05 +0800

    fixed 75, 214, 424

---
+++ 214.最短回文串.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=214 lang=cpp
+ *
+ * [214] 最短回文串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        int base = 131, mod = 1000000007;
+        int left = 0, right = 0, mul = 1;
+        int best = -1;
+        for (int i = 0; i < n; ++i) {
+            left = ((long long)left * base + s[i]) % mod;
+            right = (right + (long long)mul * s[i]) % mod;
+            if (left == right) {
+                best = i;
+            }
+            mul = (long long)mul * base % mod;
+        }
+        string add = (best == n - 1 ? "" : s.substr(best + 1, n));
+        reverse(add.begin(), add.end());
+        return add + s;
+    }
+};
+// @lc code=end
+
---
+++ 424.替换后的最长重复字符.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=424 lang=cpp
+ *
+ * [424] 替换后的最长重复字符
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> num(26);
+        int n = s.length();
+        int maxn = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            num[s[right] - 'A']++;
+            maxn = max(maxn, num[s[right] - 'A']);
+            if (right - left + 1 - maxn > k) {
+                num[s[left] - 'A']--;
+                left++;
+            }
+            right++;
+        }
+        return right - left;
+    }
+};
+// @lc code=end
+
