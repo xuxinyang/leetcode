87f239dac903887e2f4bff20946275a3026366fc
xuxinyang <1029644978@qq.com>
2022-06-17 08:21:03 +0800

    fixed 1089

--- 1089.复写零.cpp
+++ 1089.复写零.cpp
@@ -7,9 +7,27 @@
 // @lc code=start
 class Solution {
 public:
-    void duplicateZeros(vector<int>& arr) {
-
+    void insert(vector<int> & arr, int k)
+    {
+        for (int i = arr.size() - 1; i > k; i--)
+        {
+            arr[i] = arr[i-1];
+        }
+        arr[k] = 0;
     }
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0;
+        while (i < n-1) {
+            if (arr[i] == 0) {
+                insert(arr, i+1);
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+    }
+
 };
 // @lc code=end
 
