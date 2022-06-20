a3b77d8ccd45ad53751f278bd769eb3dcf34ed59
xuxinyang <1029644978@qq.com>
2022-06-20 11:16:36 +0800

    fixed 714, 721

---
+++ 714.买卖股票的最佳时机含手续费.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=714 lang=cpp
+ *
+ * [714] 买卖股票的最佳时机含手续费
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        int sell = 0, buy = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            tie(sell, buy) = pair(max(sell, buy + prices[i] - fee), max(buy, sell - prices[i]));
+        }
+        return sell;
+    }
+};
+// @lc code=end
+
