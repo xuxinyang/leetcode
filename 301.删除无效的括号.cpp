b7991d83ae29da912ceb496965f6206045f31dec
xuxinyang <1029644978@qq.com>
2022-06-20 10:51:06 +0800

    fixed 80, 211, 301

--- 301.删除无效的括号.cpp
+++ 301.删除无效的括号.cpp
@@ -7,36 +7,69 @@
 // @lc code=start
 class Solution {
 public:
+    vector<string> res;
     vector<string> removeInvalidParentheses(string s) {
-        vector<string> res;
-        dfs(s, 0, 0, res);
+        int lremove = 0;
+        int rremove = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                lremove++;
+            } else if (c == ')') {
+                if (lremove == 0) {
+                    rremove++;
+                } else {
+                    lremove--;
+                }
+            }
+        }
+        helper(s, 0, lremove, rremove);
         return res;
     }
-private:
-    void dfs(string s, int start, int count, vector<string>& res) {
-        if (count < 0) {
-            return;
-        }
-        if (start == s.size()) {
-            if (count == 0) {
-                res.push_back(s);
+
+    void helper(string str, int start, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            if (isValid(str)) {
+                res.push_back(str);
             }
             return;
         }
-        if (s[start] == '(') {
-            dfs(s, start+1, count+1, res);
-        } else if (s[start] == ')') {
-            dfs(s, start+1, count-1, res);
-        } else {
-            dfs(s, start+1, count, res);
-        }
-        if (s[start] != '(' && s[start] != ')') {
-            s[start] = '#';
-            dfs(s, start+1, count, res);
-            s[start] = s[start-1];
+        for (int i = start; i < str.size(); i++) {
+            if (i != start && str[i] == str[i - 1]) {
+                continue;
+            }
+            // 如果剩余的字符无法满足去掉的数量要求，直接返回
+            if (lremove + rremove > str.size() - i) {
+                return;
+            } 
+            // 尝试去掉一个左括号
+            if (lremove > 0 && str[i] == '(') {
+                helper(str.substr(0, i) + str.substr(i + 1), i, lremove - 1, rremove);
+            }
+            // 尝试去掉一个右括号
+            if (rremove > 0 && str[i] == ')') {
+                helper(str.substr(0, i) + str.substr(i + 1), i, lremove, rremove - 1);
+            }
         }
     }
 
+    inline bool isValid(const string & str) {
+        int cnt = 0;
+
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] == '(') {
+                cnt++;
+            } else if (str[i] == ')') {
+                cnt--;
+                if (cnt < 0) {
+                    return false;
+                }
+            }
+        }
+
+        return cnt == 0;
+    }
 };
+
 // @lc code=end
 
---
+++ 80.删除有序数组中的重复项-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=80 lang=cpp
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+};
+// @lc code=end
+
