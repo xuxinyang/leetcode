778938ec361443d6cab1a0e8ed7e1c68be427a2d
xuxinyang <1029644978@qq.com>
2022-06-23 11:20:34 +0800

    fixed 41

---
+++ 41.缺失的第一个正数.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=41 lang=cpp
+ *
+ * [41] 缺失的第一个正数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
+// @lc code=end
+
