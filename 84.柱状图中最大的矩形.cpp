76445524a007a156c574a5df93159d44064397ca
xuxinyang <1029644978@qq.com>
2022-06-24 11:30:49 +0800

    fixed 84

---
+++ 84.柱状图中最大的矩形.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=cpp
+ *
+ * [84] 柱状图中最大的矩形
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n), right(n, n);
+        
+        stack<int> mono_stack;
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.empty() && heights[mono_stack.top()] >= heights[i]) {
+                right[mono_stack.top()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.empty() ? -1 : mono_stack.top());
+            mono_stack.push(i);
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
