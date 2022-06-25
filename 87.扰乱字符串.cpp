9abcd48095ac3c8799bc7bceb1f45fa2c87fa658
xuxinyang <1029644978@qq.com>
2022-06-25 09:01:12 +0800

    fixed 86, 87

--- 87.扰乱字符串.cpp
+++ 87.扰乱字符串.cpp
@@ -6,29 +6,70 @@
 
 // @lc code=start
 class Solution {
+private:
+    // 记忆化搜索存储状态的数组
+    // -1 表示 false，1 表示 true，0 表示未计算
+    int memo[30][30][31];
+    string s1, s2;
+
 public:
+    bool checkIfSimilar(int i1, int i2, int length) {
+        unordered_map<int, int> freq;
+        for (int i = i1; i < i1 + length; ++i) {
+            ++freq[s1[i]];
+        }
+        for (int i = i2; i < i2 + length; ++i) {
+            --freq[s2[i]];
+        }
+        if (any_of(freq.begin(), freq.end(), [](const auto& entry) {return entry.second != 0;})) {
+            return false;
+        }
+        return true;
+    }
+
+    // 第一个字符串从 i1 开始，第二个字符串从 i2 开始，子串的长度为 length，是否和谐
+    bool dfs(int i1, int i2, int length) {
+        if (memo[i1][i2][length]) {
+            return memo[i1][i2][length] == 1;
+        }
+
+        // 判断两个子串是否相等
+        if (s1.substr(i1, length) == s2.substr(i2, length)) {
+            memo[i1][i2][length] = 1;
+            return true;
+        }
+
+        // 判断是否存在字符 c 在两个子串中出现的次数不同
+        if (!checkIfSimilar(i1, i2, length)) {
+            memo[i1][i2][length] = -1;
+            return false;
+        }
+        
+        // 枚举分割位置
+        for (int i = 1; i < length; ++i) {
+            // 不交换的情况
+            if (dfs(i1, i2, i) && dfs(i1 + i, i2 + i, length - i)) {
+                memo[i1][i2][length] = 1;
+                return true;
+            }
+            // 交换的情况
+            if (dfs(i1, i2 + length - i, i) && dfs(i1 + i, i2, length - i)) {
+                memo[i1][i2][length] = 1;
+                return true;
+            }
+        }
+
+        memo[i1][i2][length] = -1;
+        return false;
+    }
+
     bool isScramble(string s1, string s2) {
-        if (s1.size() != s2.size()) return false;
-        if (s1 == s2) return true;
-        int n = s1.size();
-        vector<vector<vector<bool>>> dp(n, vector<vector<bool>>(n, vector<bool>(n, false)));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j][j] = s1[i] == s2[j];
-            }
-        }
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                for (int j = 0; j + len - 1 < n; j++) {
-                    for (int k = 1; k < len; k++) {
-                        dp[i][j][i + k - 1] = dp[i][j][i + k - 1] && dp[i + k][j][j + len - 1] || dp[i][j][i + len - 1] && dp[i + k][j + len - 1][j + k - 1];
-                        dp[i][j][i + len - 1] = dp[i][j][i + len - 1] && dp[i][j + k][j + len - 1] || dp[i][j][i + k - 1] && dp[i + k][j][j + len - 1];
-                    }
-                }
-            }
-        }
-        return dp[0][0][n - 1];
+        memset(memo, 0, sizeof(memo));
+        this->s1 = s1;
+        this->s2 = s2;
+        return dfs(0, 0, s1.size());
     }
 };
+
 // @lc code=end
 
