a399c69aaa4159685e7094329e8a53f62b2f416b
xuxinyang <1029644978@qq.com>
2022-07-17 22:43:24 +0800

    fixed 376

--- 376.摆动序列.cpp
+++ 376.摆动序列.cpp
@@ -9,20 +9,24 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
-        vector<int> dp(n + 1, 0);
-        for (int i = 1; i <= n; ++i) {
-            dp[i] = dp[i - 1] + nums[i - 1];
+        if (n < 2) {
+            return n;
         }
-        vector<int> up(n + 1, 0);
-        vector<int> down(n + 1, 0);
-        up[1] = dp[1];
-        down[1] = dp[1];
-        for (int i = 2; i <= n; ++i) {
-            up[i] = max(up[i - 1], dp[i] - down[i - 1]);
-            down[i] = max(down[i - 1], dp[i] - up[i - 1]);
+        vector<int> up(n), down(n);
+        up[0] = down[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = max(up[i - 1] + 1, down[i - 1]);
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
         }
-        return max(up[n], down[n]);
+        return max(up[n - 1], down[n - 1]);
     }
 };
 // @lc code=end
