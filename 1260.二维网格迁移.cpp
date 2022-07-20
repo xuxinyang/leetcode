4b796892c822064ce0837f81904c5273073cddc5
xuxinyang <1029644978@qq.com>
2022-07-20 21:51:22 +0800

    fixed 1260

---
+++ 1260.二维网格迁移.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1260 lang=cpp
+ *
+ * [1260] 二维网格迁移
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> ans(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int ind = (i * m + j + k) % (n * m);
+                ans[ind / m][ind % m] = grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
