8f082c9cd4f35567043465605c593252cb98b963
xuxinyang <1029644978@qq.com>
2022-07-23 09:26:37 +0800

    updateq

--- 1027.最长等差数列.cpp
+++ 1027.最长等差数列.cpp
@@ -10,14 +10,15 @@ public:
     int longestArithSeqLength(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int> > dp(n, vector<int>(1001, 0));
+        // dp[i][d]表示以数组下标i处的元素结尾,公差为d的等差数列的最大长度
         int ans = 0;
-        for (int k = 1; k < n; k++)
+        for (int i = 1; i < n; i++)
         {
-            for (int j = 0; j < k; j++)
+            for (int j = 0; j < i; j++)
             {
-                int d = nums[k] - nums[j] + 500;
-                dp[k][d] = dp[j][d] + 1;
-                ans = max(ans, dp[k][d]);
+                int d = nums[i] - nums[j] + 500;
+                dp[i][d] = dp[j][d] + 1;
+                ans = max(ans, dp[i][d]);
             }
         }
         return ans+1;
