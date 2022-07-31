149bdb830c42dd2b276c9960bd78f9ff80d46cd6
xuxinyang <1029644978@qq.com>
2022-07-31 10:02:44 +0800

    fixed 352, 454

--- 454.四数相加-ii.cpp
+++ 454.四数相加-ii.cpp
@@ -7,43 +7,22 @@
 // @lc code=start
 class Solution {
 public:
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        // 初始化数组
-        unordered_map<int, int> nums1_map;
-        for (int i = 0; i < nums1.size(); ++i) {
-            nums1_map[nums1[i]]++;
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> countAB;
+        for (int u: A) {
+            for (int v: B) {
+                ++countAB[u + v];
+            }
         }
-        
-        unordered_map<int, int> nums2_map;
-        for (int i = 0; i < nums2.size(); ++i) {
-            nums2_map[nums2[i]]++;
-        }
-        
-        unordered_map<int, int> nums3_map;
-        for (int i = 0; i < nums3.size(); ++i) {
-            nums3_map[nums3[i]]++;
-        }
-        
-        unordered_map<int, int> nums4_map;
-        for (int i = 0; i < nums4.size(); ++i) {
-            nums4_map[nums4[i]]++;
-        }
-        
-        // 开始求解
-        int res = 0;
-        for (int i = 0; i < nums1.size(); ++i) {
-            for (int j = 0; j < nums2.size(); ++j) {
-                for (int k = 0; k < nums3.size(); ++k) {
-                    for (int l = 0; l < nums4.size(); ++l) {
-                        if (nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0) {
-                            res += nums1_map[nums1[i]] * nums2_map[nums2[j]] * nums3_map[nums3[k]] * nums4_map[nums4[l]];
-                        }
-                    }
+        int ans = 0;
+        for (int u: C) {
+            for (int v: D) {
+                if (countAB.count(-u - v)) {
+                    ans += countAB[-u - v];
                 }
             }
         }
-        
-        return res;
+        return ans;
     }
 };
 // @lc code=end
