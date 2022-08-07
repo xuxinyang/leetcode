fe859ecd4693e85f9b0a409b2d89295ec8eda1d0
xuxinyang <1029644978@qq.com>
2022-08-07 09:36:56 +0800

    fixed 892

--- 892.三维形体的表面积.cpp
+++ 892.三维形体的表面积.cpp
@@ -5,27 +5,31 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int surfaceArea(vector<vector<int>>& grid) {
+    int surfaceArea(vector<vector<int>> &grid)
+    {
         int n = grid.size();
-        if (n == 0) return 0;
         int m = grid[0].size();
-        if (m == 0) return 0;
         int res = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (grid[i][j] > 0) {
-                    res += 2;
-                    if (i > 0) res -= min(grid[i][j], grid[i - 1][j]);
-                    if (j > 0) res -= min(grid[i][j], grid[i][j - 1]);
-                    if (i < n - 1) res -= min(grid[i][j], grid[i + 1][j]);
-                    if (j < m - 1) res -= min(grid[i][j], grid[i][j + 1]);
-                }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                // 前面的面积
+                res += (i - 1) >= 0 ? max(grid[i][j] - grid[i - 1][j], 0) : grid[i][j];
+                // 后面的面积
+                res += (i + 1) < n ? max(grid[i][j] - grid[i + 1][j], 0) : grid[i][j];
+                // 左侧的面积
+                res += (j - 1) >= 0 ? max(grid[i][j] - grid[i][j - 1], 0) : grid[i][j];
+                // 右侧的面积
+                res += (j + 1) < m ? max(grid[i][j] - grid[i][j + 1], 0) : grid[i][j];
+                // 上下的面积
+                res += 2 * (grid[i][j] > 0 ? 1 : 0);
             }
         }
         return res;
     }
 };
 // @lc code=end
-
