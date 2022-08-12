a3c7be3be36b5c23d2a3d3f6e822ff8ead2ed778
xuxinyang <1029644978@qq.com>
2022-08-12 11:32:02 +0800

    fixed 1023

--- 1023.驼峰式匹配.cpp
+++ 1023.驼峰式匹配.cpp
@@ -5,16 +5,38 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+    vector<bool> camelMatch(vector<string> &queries, string pattern)
+    {
         vector<bool> res;
-        for (auto& query : queries) {
+        for (auto &query : queries)
+        {
             res.push_back(match(query, pattern));
         }
         return res;
     }
-    
+    bool match(string &query, string &pattern)
+    {
+        int i = 0, j = 0;
+        while (i < query.size())
+        {
+            if (query[i] == pattern[j] && j < pattern.size())
+            {
+                i++;
+                j++;
+            }
+            else if (isupper(query[i]))
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return i == query.size() && j == pattern.size();
+    }
 };
 // @lc code=end
-
