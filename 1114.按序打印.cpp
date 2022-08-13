7624bd2c7c90dd06bc49d2509e31da05d6324fa7
xuxinyang <1029644978@qq.com>
2022-08-13 13:57:22 +0800

    fixed 1114

--- 1114.按序打印.cpp
+++ 1114.按序打印.cpp
@@ -5,27 +5,40 @@
  */
 
 // @lc code=start
-class Foo {
+class Foo
+{
+    mutex mtx1, mtx2;
+
 public:
-    Foo() {
+    Foo()
+    {
+        mtx1.lock(), mtx2.lock();
     }
 
-    void first(function<void()> printFirst) {
+    void first(function<void()> printFirst)
+    {
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
+        mtx1.unlock();
     }
 
-    void second(function<void()> printSecond) {
-        
+    void second(function<void()> printSecond)
+    {
+
         // printSecond() outputs "second". Do not change or remove this line.
+        mtx1.lock();
         printSecond();
+        mtx1.unlock();
+        mtx2.unlock();
     }
 
-    void third(function<void()> printThird) {
-        
+    void third(function<void()> printThird)
+    {
+
         // printThird() outputs "third". Do not change or remove this line.
+        mtx2.lock();
         printThird();
+        mtx2.unlock();
     }
 };
 // @lc code=end
-
