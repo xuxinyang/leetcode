dabba3123943aa6d14c8bbdd86678ebbef9f2ba8
xuxinyang <1029644978@qq.com>
2022-08-15 22:07:43 +0800

    fixed 752

--- 752.打开转盘锁.cpp
+++ 752.打开转盘锁.cpp
@@ -5,33 +5,60 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int openLock(vector<string>& deadends, string target) {
+    int openLock(vector<string> &deadends, string target)
+    {
+        if (target == "0000")
+            return 0;
         unordered_set<string> dead(deadends.begin(), deadends.end());
-        if (dead.count("0000") || dead.count(target)) return -1;
-        queue<string> q;
-        q.push("0000");
-        int step = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                string cur = q.front();
-                q.pop();
-                if (cur == target) return step;
-                if (dead.count(cur)) continue;
-                for (int i = 0; i < 4; i++) {
-                    string next = cur;
-                    next[i] = (next[i] - '0' + 1) % 10 + '0';
-                    if (next != cur) q.push(next);
-                    next[i] = (next[i] - '0' - 1 + 10) % 10 + '0';
-                    if (next != cur) q.push(next);
+        if (dead.count("0000"))
+            return -1;
+        auto num_prev = [](char x) -> char
+        {
+            return (x == '0' ? '9' : x - 1);
+        };
+        auto num_succ = [](char x) -> char
+        {
+            return (x == '9' ? '0' : x + 1);
+        };
+
+        auto get = [&](string status) -> vector<string>
+        {
+            vector<string> ret;
+            for (int i = 0; i < 4; i++)
+            {
+                char num = status[i];
+                status[i] = num_prev(num);
+                ret.push_back(status);
+                status[i] = num_succ(num);
+                ret.push_back(status);
+                status[i] = num;
+            }
+            return ret;
+        };
+        queue<pair<string, int>> q;
+        q.emplace("0000", 0);
+        unordered_set<string> seen = {"0000"};
+        while (!q.empty())
+        {
+            auto [status, step] = q.front();
+            q.pop();
+            for (auto &&next_status : get(status))
+            {
+                if (!seen.count(next_status) && !dead.count(next_status))
+                {
+                    if (next_status == target)
+                    {
+                        return step + 1;
+                    }
+                    q.emplace(next_status, step + 1);
+                    seen.insert(move(next_status));
                 }
             }
-            step++;
         }
         return -1;
     }
 };
 // @lc code=end
-
