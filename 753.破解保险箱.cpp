112dfb81919e11b100820d886f895b7103049e85
xuxinyang <1029644978@qq.com>
2022-08-21 09:18:49 +0800

    fixed 753

---
+++ 753.破解保险箱.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=753 lang=cpp
+ *
+ * [753] 破解保险箱
+ */
+
+// @lc code=start
+class Solution
+{
+private:
+    unordered_set<int> seen;
+    string ans;
+    int highest;
+    int k;
+
+public:
+    void dfs(int node)
+    {
+        for (int x = 0; x < k; ++x)
+        {
+            int nei = node * 10 + x;
+            if (!seen.count(nei))
+            {
+                seen.insert(nei);
+                dfs(nei % highest);
+                ans += (x + '0');
+            }
+        }
+    }
+
+    string crackSafe(int n, int k)
+    {
+        highest = pow(10, n - 1);
+        this->k = k;
+        dfs(0);
+        ans += string(n - 1, '0');
+        return ans;
+    }
+};
+// @lc code=end
