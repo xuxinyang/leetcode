503991b0d4241511ee5b477f7562009fdfe1fc5a
xuxinyang <1029644978@qq.com>
2022-08-23 21:13:17 +0800

    fixed 782, 949

--- 949.给定数字能组成的最大时间.cpp
+++ 949.给定数字能组成的最大时间.cpp
@@ -5,19 +5,38 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string largestTimeFromDigits(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        string res = "";
-        do {
-            string s = to_string(arr[0]) + to_string(arr[1]) + ":" + to_string(arr[2]) + to_string(arr[3]);
-            if (s > res) {
-                res = s;
-            }
-        } while (next_permutation(arr.begin(), arr.end()));
-        return res;
+    string largestTimeFromDigits(vector<int> &A)
+    {
+        int res = -1;
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                if (j != i)
+                    for (int k = 0; k < 4; k++)
+                        if (k != i && k != j)
+                        {
+                            int l = 6 - i - j - k; //确定l的位置  6=0+1+2+3
+                            int hours = 10 * A[i] + A[j];
+                            int mins = 10 * A[k] + A[l];
+                            if (hours < 24 && mins < 60)
+                                res = max(res, hours * 60 + mins);
+                        }
+        if (res >= 0)
+        {
+            string str;
+            if (res / 60 < 10)
+                str += '0';
+            str += to_string(res / 60);
+            str += ':';
+            if (res % 60 < 10)
+                str += '0';
+            str += to_string(res % 60);
+            return str;
+        }
+        return "";
     }
 };
-// @lc code=end
 
+// @lc code=end
