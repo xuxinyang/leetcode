d27e3e8b8dde7f72c80c94db2c3f677709a852fa
xuxinyang <1029644978@qq.com>
2022-09-29 19:34:00 +0800

    fixed 993

--- 993.二叉树的堂兄弟节点.cpp
+++ 993.二叉树的堂兄弟节点.cpp
@@ -17,29 +17,52 @@
  * };
  */
 class Solution {
+private:
+    // x 的信息
+    int x;
+    TreeNode* x_parent;
+    int x_depth;
+    bool x_found = false;
+
+    // y 的信息
+    int y;
+    TreeNode* y_parent;
+    int y_depth;
+    bool y_found = false;
+
 public:
-    bool isCousins(TreeNode* root, int x, int y) {
-        if (!root) return false;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                if (node->left && node->right) {
-                    if (node->left->val == x && node->right->val == y) {
-                        return false;
-                    }
-                    if (node->left->val == y && node->right->val == x) {
-                        return false;
-                    }
-                }
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
+    void dfs(TreeNode* node, int depth, TreeNode* parent) {
+        if (!node) {
+            return;
         }
-        return true;
+
+        if (node->val == x) {
+            tie(x_parent, x_depth, x_found) = tuple{parent, depth, true};
+        }
+        else if (node->val == y) {
+            tie(y_parent, y_depth, y_found) = tuple{parent, depth, true};
+        }
+
+        // 如果两个节点都找到了，就可以提前退出遍历
+        // 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+        if (x_found && y_found) {
+            return;
+        }
+
+        dfs(node->left, depth + 1, node);
+
+        if (x_found && y_found) {
+            return;
+        }
+
+        dfs(node->right, depth + 1, node);
+    }
+
+    bool isCousins(TreeNode* root, int x, int y) {
+        this->x = x;
+        this->y = y;
+        dfs(root, 0, nullptr);
+        return x_depth == y_depth && x_parent != y_parent;
     }
 };
 // @lc code=end
