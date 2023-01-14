03d90993651678e64e17c27b8f913def965b6b3a
xuxinyang <1029644978@qq.com>
2023-01-14 19:09:45 +0800

    update 123

--- 123.买卖股票的最佳时机-iii.cpp
+++ 123.买卖股票的最佳时机-iii.cpp
@@ -9,13 +9,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int buy1 = -prices[0], sell1 = 0;
-        int buy2 = -prices[0], sell2 = 0;
+        int buy1 = prices[0], sell1 = 0;
+        int buy2 = prices[0], sell2 = 0;
         for (int i = 1; i < n; ++i) {
-            buy1 = max(buy1, -prices[i]);
-            sell1 = max(sell1, buy1 + prices[i]);
-            buy2 = max(buy2, sell1 - prices[i]);
-            sell2 = max(sell2, buy2 + prices[i]);
+            buy1 = min(buy1, prices[i]);
+            sell1 = max(sell1, prices[i] - buy1);
+            buy2 = min(buy2, prices[i] - sell1);
+            sell2 = max(sell2, prices[i]-buy2);
         }
         return sell2;
     }
