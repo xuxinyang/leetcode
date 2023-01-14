595eaa2ce508d2fd5faaa00988c8555293d6dc07
xuxinyang <1029644978@qq.com>
2023-01-14 18:30:32 +0800

    fixed 518

---
+++ 518.零钱兑换-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1);
+        dp[0] = 1;
+        for (int i = 0; i < coins.size(); i++)
+        {
+            for (int j = 1; j <= amount; j++)
+            {
+                if (j - coins[i] >= 0)
+                {
+                    dp[j] += dp[j-coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
+// @lc code=end
+
