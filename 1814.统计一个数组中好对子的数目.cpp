5c10638b15d98462771af8b979a1b771c8bd71ad
xuxinyang <1029644978@qq.com>
2023-01-17 12:51:18 +0800

    fixed 1814

---
+++ 1814.统计一个数组中好对子的数目.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1814 lang=cpp
+ *
+ * [1814] 统计一个数组中好对子的数目
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        const int MOD = 1e9+7;
+        int res = 0;
+        unordered_map<int, int> h;
+        for (int i: nums) {
+            int temp = i, j = 0;
+            while (temp)
+            {
+                j = j * 10 + temp % 10;
+                j /= 10;
+            }
+            res = (res + h[i-j]) % MOD;
+            h[i-j] ++;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
